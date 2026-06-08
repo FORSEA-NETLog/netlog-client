adcdf6ee58e58d171a6f83e5a4110adfa7c2897d
kayunni <gykim0129@gmail.com>
2026-06-08 11:08:45 +0900

    fix: 라우팅 구조 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
+import LandingPage from './pages/LandingPage'
+import MapPage from './pages/MapPage'
+import CheckerLoginPage from './pages/CheckerLoginPage'
 import CheckerPage from './pages/CheckerPage'
-import DashboardPage from './pages/DashboardPage'
 import CompletePage from './pages/CompletePage'
+import DashboardLoginPage from './pages/DashboardLoginPage'
+import DashboardPage from './pages/DashboardPage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/checker" element={<CheckerPage />} />
-        <Route path="/complete" element={<CompletePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+        {/* 공개 */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/map" element={<MapPage />} />
+
+        {/* 검수자 */}
+        <Route path="/checker/login" element={<CheckerLoginPage />} />
+        <Route path="/checker" element={
+          <ProtectedRoute role="checker"><CheckerPage /></ProtectedRoute>
+        } />
+        <Route path="/checker/complete" element={
+          <ProtectedRoute role="checker"><CompletePage /></ProtectedRoute>
+        } />
+
+        {/* 관리자 */}
+        <Route path="/dashboard/login" element={<DashboardLoginPage />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute role="admin"><DashboardPage /></ProtectedRoute>
+        } />
       </Routes>
     </BrowserRouter>
   )
